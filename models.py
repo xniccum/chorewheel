d56e025c20fb2497f470966ae561c17f81f1ace1
xniccum <xniccum@gmail.com>
2016-10-24 22:28:11 -0400

    finished chores

--- models.py
+++ models.py
@@ -4,11 +4,13 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     email = ndb.StringProperty()
     groups = ndb.KeyProperty(kind='Group', repeated=True)
-    
+
+
 class Group(ndb.Model):
     admins = ndb.KeyProperty(kind='User', repeated=True)
     members = ndb.KeyProperty(kind='User', repeated=True)
     name = ndb.StringProperty()
+
 
 class Chore(ndb.Model):
     name = ndb.StringProperty()
