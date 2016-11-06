b150fba75f7397e10dc6bb8464ab91c11d58b48c
xniccum <xniccum@gmail.com>
2016-11-06 02:38:37 -0500

    added url email invite

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,3 +1,6 @@
+from google.appengine.api import app_identity
+from google.appengine.api import mail
+
 from handlers import base_handlers
 from google.appengine.ext import ndb
 from models import User
@@ -7,10 +10,30 @@ class InsertMember(base_handlers.BaseAction):
     def handle_post(self, google_user):
         group_key = ndb.Key(urlsafe=self.request.get("group-key"))
         group = group_key.get()
-        if self.request.get("member-key"):
-            pass
+        if self.request.get("send"):
+            email = self.request.get("member-email")
+            if mail.is_email_valid(email):
+                url = "http://chore-wheel-project.appspot.com/invite?"
+                url += "group-key="+self.request.get("group-key")+"&"
+                url += "email="+self.request.get("member-email")+"&"
+                url += "admin="+self.request.get("member-admin")
+
+                sender_address = ('Chore-Wheel.com Support <{}@appspot.gserviceaccount.com>'.format(app_identity.get_application_id()))
+                subject = "Chore-Wheel Invitation"
+                body="""You have been invited to join the {} Chore-Wheel group
+                If you wish to join this group, go to:
+
+                {}""".format(group.name, url)
+                mail.send_mail(sender_address, email, subject, body)
+            else:
+                pass
+        elif self.request.get("member-key"):
             member_key = ndb.Key(urlsafe=self.request.get("member-key"))
-            member = member_key.get()
+            if bool(self.request.get("member-admin")):
+                group.admins.append(member_key)
+            else:
+                group.admins.remove(member_key)
+            group.put()
         else:
             member = User.query(ancestor=User.PARENT_KEY).filter(User.email == self.request.get("member-email")).get()
             if not member:
